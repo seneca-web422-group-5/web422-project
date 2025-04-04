95a4a6a61b79bc36bded56546a371e240a7bb44f
Arin <arin.apichitchon@gmail.com>
2025-04-04 16:47:26 -0400

    fixed layout

--- tasty-application/src/components/PopularCategory.jsx
+++ tasty-application/src/components/PopularCategory.jsx
@@ -13,9 +13,9 @@ const PopularCategory = ({ categories }) => {
   return (
     <div className="popular-category-container">
       <h2 className="mb-4">Popular Category</h2>
-      <div className="category-list">
+      <div className="category-list d-flex justify-content-center">
         {categories.map((category, index) => (
-          <div key={index} className="category-item">
+          <div key={index} className="category-item ms-2">
             {/* Check if the category has an image */}
             {categoryImages[category.name] ? (
               <img
