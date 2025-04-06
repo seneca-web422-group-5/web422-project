f2bf670e4d1ea590e36e3bf7ebcee8753e30c6aa
Arin <arin.apichitchon@gmail.com>
2025-04-06 15:55:39 -0400

    checkpoint

--- tasty-application/src/components/SearchBar.jsx
+++ tasty-application/src/components/SearchBar.jsx
@@ -2,27 +2,28 @@ import React, { useState, useCallback } from 'react'
 import { getAutoCompleteSuggestions } from '../lib/api'
 import debounce from 'lodash/debounce'
 
+const debouncedFetchSuggestions = debounce(async (value, setSuggestions) => {
+  if (value.length > 2) {
+    try {
+      const data = await getAutoCompleteSuggestions(value)
+      console.log('Auto-complete Suggestions:', data)
+      setSuggestions(data?.results || [])
+    } catch (err) {
+      console.error('Error fetching auto-complete suggestions:', err)
+    }
+  } else {
+    setSuggestions([])
+  }
+}, 300) // 300ms debounce delay
+
 const SearchBar = () => {
   const [query, setQuery] = useState('')
   const [suggestions, setSuggestions] = useState([])
 
-  // Debounced function to fetch suggestions
-  const fetchSuggestions = useCallback(
-    debounce(async (value) => {
-      if (value.length > 2) {
-        try {
-          const data = await getAutoCompleteSuggestions(value)
-          console.log('Auto-complete Suggestions:', data)
-          setSuggestions(data?.results || [])
-        } catch (err) {
-          console.error('Error fetching auto-complete suggestions:', err)
-        }
-      } else {
-        setSuggestions([])
-      }
-    }, 300), // 300ms debounce delay
-    []
-  )
+  // Memoize the function to avoid recreating it on every render
+  const fetchSuggestions = useCallback((value) => {
+    debouncedFetchSuggestions(value, setSuggestions)
+  }, [])
 
   const handleInputChange = (e) => {
     const value = e.target.value
