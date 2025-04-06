ddbd8b10ae873cc4393cd78f4e15049505c7ec14
Arin <arin.apichitchon@gmail.com>
2025-04-06 15:31:27 -0400

    add onCLick

--- tasty-application/src/components/LatestRecipes.jsx
+++ tasty-application/src/components/LatestRecipes.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './LatestRecipes.css'
 
 const LatestRecipes = ({ recipes }) => {
+  const navigate = useNavigate()
   // Sort recipes by created_at in descending order
   const sortedRecipes = [...recipes].sort((a, b) => b.created_at - a.created_at)
+
+  const handleRecipeClick = (id) => {
+    if (id) {
+      navigate(`/recipe/${id}`)
+    }
+  }
 
   return (
     <div className="latest-recipes-container">
       <h2 className="mb-4 text-center">Latest Recipes</h2>
       <div className="recipes-list row justify-content-center">
         {sortedRecipes.map((recipe) => (
-          <div key={recipe.id} className="recipe-item col-6 col-sm-4 col-md-3 col-lg-2 text-center mb-4">
+          <div key={recipe.id} className="recipe-item col-6 col-sm-4 col-md-3 col-lg-2 text-center mb-4" onClick={() => handleRecipeClick(recipe.id)} style={{ cursor: 'pointer' }}>
             {/* Recipe Thumbnail */}
             {recipe.thumbnail_url ? (
               <img
