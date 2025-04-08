f1413bc00fc324837c5a1022875337139f38285b
Arin <arin.apichitchon@gmail.com>
2025-04-08 02:37:32 -0400

    Revert "Revert "Revert "Revert "Category""""
    This reverts commit 0410dbdae9cdc4abf93c5f20f982e22e76ac2281.

--- tasty-application/src/App.js
+++ tasty-application/src/App.js
@@ -3,13 +3,16 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import AuthProvider from './context/AuthContext'
 import Layout from './app/layout'
+import { DataCacheProvider } from './context/DataCacheContext'
 
 const App = () => {
   return (
     <AuthProvider>
-      <Header />
-      <Layout />
-      <Footer />
+      <DataCacheProvider>
+        <Header />
+          <Layout />
+        <Footer />
+      </DataCacheProvider>      
     </AuthProvider>
   )
 }
