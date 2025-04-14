eaca15a2237e0f491ed41e5fbd29b1bd3e6f287a
Arin <arin.apichitchon@gmail.com>
2025-04-14 09:24:20 -0400

    debug

--- tasty-application/src/lib/api.js
+++ tasty-application/src/lib/api.js
@@ -6,6 +6,7 @@ const BASE_URL = 'https://tasty.p.rapidapi.com'
 const API_KEY = process.env.REACT_APP_API_KEY // Store securely (e.g., env variables)
 
 const fetchFromTastyAPI = async (endpoint, params = {}) => {
+  console.log(API_KEY)
   const url = new URL(`${BASE_URL}/${endpoint}`)
   Object.keys(params).forEach((key) => url.searchParams.append(key, params[key]))
 
@@ -30,33 +31,38 @@ const fetchFromTastyAPI = async (endpoint, params = {}) => {
 }
 
 // Export functions
-export const getRecipeDetails = (id) => fetchFromTastyAPI("recipes/detail", { id });
-export const searchRecipes = (query = "", from = 0, size = 20) =>  fetchFromTastyAPI("recipes/list", { q: query, from, size });
-export const getAutoComplete = (query) => fetchFromTastyAPI("recipes/auto-complete", { prefix: query });
-export const getTags = (query) => fetchFromTastyAPI("tags/list");
-export const getAutoCompleteSuggestions = (query) =>  fetchFromTastyAPI('recipes/auto-complete', { prefix: query })
-export const getPopularCategories = async () => {  return fetchFromTastyAPI('tags/list')}
-export const getFeeds = (size = 1, timezone = '+0700', from = 0) =>  fetchFromTastyAPI('feeds/list', { size, timezone, from })
-export const getLatestRecipes = (from = 0, size = 100, tags = 'under_30_minutes') =>  fetchFromTastyAPI('recipes/list', { from, size, tags })
+export const getRecipeDetails = (id) => fetchFromTastyAPI('recipes/detail', { id })
+export const searchRecipes = (query = '', from = 0, size = 20) =>
+  fetchFromTastyAPI('recipes/list', { q: query, from, size })
+export const getAutoComplete = (query) =>
+  fetchFromTastyAPI('recipes/auto-complete', { prefix: query })
+export const getTags = (query) => fetchFromTastyAPI('tags/list')
+export const getAutoCompleteSuggestions = (query) =>
+  fetchFromTastyAPI('recipes/auto-complete', { prefix: query })
+export const getPopularCategories = async () => {
+  return fetchFromTastyAPI('tags/list')
+}
+export const getFeeds = (size = 1, timezone = '+0700', from = 0) =>
+  fetchFromTastyAPI('feeds/list', { size, timezone, from })
+export const getLatestRecipes = (from = 0, size = 100, tags = 'under_30_minutes') =>
+  fetchFromTastyAPI('recipes/list', { from, size, tags })
 export const getSimilarRecipes = (recipeId) =>
   fetchFromTastyAPI('recipes/list-similarities', { recipe_id: recipeId })
-export const getFullDetail = (id) => fetchFromTastyAPI(`recipes/get-more-info`, { id });
-
+export const getFullDetail = (id) => fetchFromTastyAPI(`recipes/get-more-info`, { id })
 
 // Assign the object to a variable
 const api = {
-    getRecipeDetails,
-    searchRecipes,
-    getAutoComplete,
-    getTags,
-    getAutoCompleteSuggestions,
-    getPopularCategories,
-    getFeeds,
-    getLatestRecipes,
-    getTags,
-    getFullDetail,
-    getSimilarRecipes
-  };
-  
-export default api;
+  getRecipeDetails,
+  searchRecipes,
+  getAutoComplete,
+  getTags,
+  getAutoCompleteSuggestions,
+  getPopularCategories,
+  getFeeds,
+  getLatestRecipes,
+  getTags,
+  getFullDetail,
+  getSimilarRecipes
+}
 
+export default api
